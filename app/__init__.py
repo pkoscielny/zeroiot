02a6d73b2f775f962604b29d4ae8646e2b2b3a14
Pawel Koscielny <mosiadz@gmail.com>
2021-02-19 19:40:39 +0100

    Switch from Flask RESTful to Flask REST JSONAPI. Improvements in README. Fixes in Dockerfile.

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,13 @@
-"""
-TODO:
-* sprobowac zrobic bardziej zaawansowana wersje listy, z filtrami, sortem, etc.
-* dodac wersje kodu
-* dodac unit testy
-* dodac "POD"
-* ? pip3 freeze > requirements.txt
-"""
 
-# PyCharm:
-# FLASK_APP=app:create_app('dev') FLASK_ENV=development python -m flask run --port=3000
-
-# Flask uses simplejson (or json from stdlib if not found) but flask_restful uses json from stdlib always.
-# It is because simplejson contains some extra things which are not in JSON standard.
-# Also Flask uses jsonify e.g. for sorting keys (better cooperation with HTTP caches).
-# I don't need caching this content so I decided to leave it as simple as possible (jsonification by flask_restful).
-# Consider using jsonify if e.g. I need jsonify more complex structures:
-#  https://flask.palletsprojects.com/en/1.1.x/api/#module-flask.json
+__version__ = "0.4.0"
 
 from flask import Flask
-from flask_restful import Api
 from app.models import db
-from app.resources import ResourceAirState, ResourceInsolation
+from app.resources import ResourceAirStateList, ResourceAirStateDetail, ResourceInsolationList, ResourceInsolationDetail
 from app.config import config
+from flask_rest_jsonapi import Api
 
 
-# Flask will automatically detect the factory (create_app or make_app) in this app.
 def create_app(stage):
     if stage not in config.keys():
         stage = 'prod'
@@ -40,7 +23,9 @@ def create_app(stage):
 
     # Create an API.
     api = Api(app)
-    api.add_resource(ResourceAirState, '/air_state')
-    api.add_resource(ResourceInsolation, '/insolation')
+    api.route(ResourceAirStateList, 'air_state_list', '/air_state')
+    api.route(ResourceAirStateDetail, 'air_state_detail', '/air_state/<int:id>')
+    api.route(ResourceInsolationList, 'insolation_list', '/insolation')
+    api.route(ResourceInsolationDetail, 'insolation_detail', '/insolation/<int:id>')
 
     return app
--- app/tests/test_resource_insolation.py
+++ app/tests/test_resource_insolation.py
@@ -1,50 +1,44 @@
 #TODO: add checking parameters.
 #TODO: write helper for recursively json checking or find relevant module in internet.
 #TODO: add more tests for list
+#TODO: add tests for GET/PUT/DELETE.
 
 def test_no_insolations(client):
-    res = client.get('/insolation')
+    with client:
+        res = client.get('/insolation')
+
     assert 200 == res.status_code
     assert b'[]' in res.data
 
 
 def test_add_new_insolation(client):
-    res = client.post(
-        '/insolation',
-        json = {'insolation': 42, 'device': 'dev2_esp'}
-    )
-    assert 201 == res.status_code
-    res_json = res.get_json()
-    assert res_json['id'] == 1
+    with client:
+        res = client.post(
+            '/insolation',
+            json={
+                'data': {
+                    'type': 'insolation',
+                    'attributes': {
+                        'insolation': 42,
+                        'device': 'dev2_esp',
+                    },
+                },
+            },
+            content_type='application/vnd.api+json',
+        )
+        assert 201 == res.status_code
+        res_json = res.get_json()
+        assert res_json['data']['type'] == 'insolation'
+        assert res_json['data']['id'] == '1'
+        assert res_json['data']['attributes']['insolation'] == 42
+        assert res_json['data']['attributes']['device'] == 'dev2_esp'
+        assert 'created' in res_json['data']['attributes'].keys()
 
-    res = client.get('/insolation')
-    assert 200 == res.status_code
-    res_json = res.get_json()
-    assert res_json[0]['id'] == 1
-    assert res_json[0]['insolation'] == 42
-    assert res_json[0]['device'] == 'dev2_esp'
-    assert len(res_json) == 1
-
-    # This routing is not accessible. Only 'list' and 'add' actions work.
-    res = client.get('/insolation/1')
-    assert 404 == res.status_code
-
-
-def test_not_accessible_routing_insolation(client):
-    res = client.post(
-        '/insolation',
-        json = {'insolation': 42, 'device': 'dev2_esp'}
-    )
-    assert 201 == res.status_code
-    res_json = res.get_json()
-    assert res_json['id'] == 1
-
-    res = client.get('/insolation/1')
-    assert 404 == res.status_code
-
-    res = client.put('/insolation/1')
-    assert 404 == res.status_code
-
-    res = client.delete('/insolation/1')
-    assert 404 == res.status_code
-
+        res = client.get('/insolation')
+        assert 200 == res.status_code
+        res_json = res.get_json()
+        assert res_json['data'][0]['id'] == '1'
+        assert res_json['data'][0]['attributes']['insolation'] == 42
+        assert res_json['data'][0]['attributes']['device'] == 'dev2_esp'
+        assert 'created' in res_json['data'][0]['attributes'].keys()
+        assert len(res_json['data']) == 1
